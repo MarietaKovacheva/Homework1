fcad63659a09692bf68f7f95492636a5173d0260
MarietaKovachava <eti_pk@mail.bg>
2022-01-31 00:09:21 +0200

    Initial commit

---
+++ src/test/java/com/kovachevamarieta/main.java
@@ -0,0 +1,13 @@
+package com.kovachevamarieta;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("homework");
+        System.out.println("enter your name");
+        Scanner scanner =new Scanner(System.in);
+        String name = scanner.next();
+    }
+
+}
